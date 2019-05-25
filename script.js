a779ae0a662ee3593a924a02f0875b6bd7f90c12
strvworks <strv13570@gmail.com>
2019-05-26 08:20:03 +0900

    見た目雛形

---
+++ script.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: "#app",
+    data: {
+        normal_use: false,
+        walk_field: false,
+        every_turn: false,
+        in_attack: false,
+        in_be_attacked: false
+    }
+});
+
