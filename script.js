ccc79a5c9db20a190c73d9ef66b08a2fbf15b1f8
strvworks <strv13570@gmail.com>
2019-05-26 09:31:49 +0900

    うん

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ new Vue({
         walk_field: false,
         every_turn: false,
         in_attack: false,
-        in_be_attacked: false
+        in_be_attacked: false,
+        json: "",
+
+        normal_use_common_atp: 0,
+        normal_use_common_hp: 0,
+
+        normal_infield_atp: 0,
+        normal_infield_hp: 0,
+        normal_infield_dangerlevel: 0,
+        normal_infield_accuracy: 0,
+        normal_infield_avoidance: 0,
+        normal_infield_attack: 0,
+        normal_infield_agility: 0,
+        normal_infield_magicdefence: 0,
+        normal_infield_physicaldefence: 0,
+        normal_infield_target: "",
+    },
+    methods: {
+        GenerateJson: function () {
+            let effects_dict = {};
+        },
+        LoadJson: function () {
+            console.log(JSON.parse(this.json));
+        }
     }
 });
 
